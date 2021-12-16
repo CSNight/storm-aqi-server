c1fd5efe06639377f1e34e209579b0952cf921d8
csnight <923739637@qq.com>
2021-12-16 23:21:56 +0800

    add es common struct

--- db/commons.go
+++ db/commons.go
@@ -18,10 +18,9 @@ type EsGetResponse struct {
 	SeqNo       int    `json:"_seq_no"`
 	PrimaryTerm int    `json:"_primary_term"`
 	Found       bool   `json:"found"`
-	Source      string `json:"_source"`
 }
 
-type EsSearchResponse struct {
+type EsSearchRespMeta struct {
 	Took     int  `json:"took"`
 	TimedOut bool `json:"timed_out"`
 	Shards   struct {
@@ -30,12 +29,16 @@ type EsSearchResponse struct {
 		Skipped    int `json:"skipped"`
 		Failed     int `json:"failed"`
 	} `json:"_shards"`
-	Hits struct {
-		Total struct {
-			Value    int    `json:"value"`
-			Relation string `json:"relation"`
-		} `json:"total"`
-		MaxScore float64  `json:"max_score"`
-		Hits     []string `json:"hits"`
-	} `json:"hits"`
+}
+
+type EsSearchItem struct {
+	Index string  `json:"_index"`
+	Type  string  `json:"_type"`
+	Id    string  `json:"_id"`
+	Score float64 `json:"_score"`
+}
+
+type EsRespTotal struct {
+	Value    int    `json:"value"`
+	Relation string `json:"relation"`
 }
