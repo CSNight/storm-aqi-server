aabfbe0b3ca25d5c9384b9da4d989e84b131ee31
csnight <923739637@qq.com>
2021-12-16 23:22:57 +0800

    add more router

--- server/server_router.go
+++ server/server_router.go
@@ -3,7 +3,10 @@ package server
 import "github.com/gofiber/fiber/v2"
 
 func (app *AQIServer) Register(root fiber.Router) {
-	root.Get("/stationById", app.GetStationById)
-	root.Get("/stationByName", app.GetStationByName)
-	root.Get("/stationByCity", app.GetStationByName)
+	root.Get("/station/@:sid", app.GetStationById)
+	root.Get("/station/name/_get", app.GetStationByName)
+	root.Get("/station/city/_get", app.GetStationByCityName)
+	root.Get("/station/name/_search", app.SearchStationByName)
+	root.Get("/station/city/_search", app.SearchStationByCityName)
+	root.Get("/station/fuzzy/_search", app.SearchStationByCityName)
 }
