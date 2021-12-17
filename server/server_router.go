dd26561abb3de3f3092f7ecf82e08bf9873be044
chensi1 <chensi1@sensetime.com>
2021-12-17 14:39:14 +0800

    simplify router

--- server/server_router.go
+++ server/server_router.go
@@ -3,10 +3,6 @@ package server
 import "github.com/gofiber/fiber/v2"
 
 func (app *AQIServer) Register(root fiber.Router) {
-	root.Get("/station/@:sid", app.GetStationById)
-	root.Get("/station/name/_get", app.GetStationByName)
-	root.Get("/station/city/_get", app.GetStationByCityName)
-	root.Get("/station/name/_search", app.SearchStationByName)
-	root.Get("/station/city/_search", app.SearchStationByCityName)
-	root.Get("/station/fuzzy/_search", app.SearchStationByCityName)
+	root.Get("/station", app.StationGet)
+	root.Get("/stations", app.StationSearch)
 }
