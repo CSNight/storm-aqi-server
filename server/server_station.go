f3752ac580bf13c3583b2abf4f9e94be92075e1c
chensi1 <chensi1@sensetime.com>
2021-12-17 14:39:47 +0800

    add request struct and dispatch request

--- server/server_station.go
+++ server/server_station.go
@@ -2,95 +2,115 @@ package server
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"reflect"
-	"strconv"
+	"net/http"
 )
 
-func (app *AQIServer) GetStationById(ctx *fiber.Ctx) error {
-	sid := ctx.Params("sid")
-	valid, err := paramsCheck(sid, ctx, reflect.String)
-	if !valid {
-		return err
+type StationGetRequest struct {
+	QType string `json:"qtype" validate:"required,oneof=_get"`
+	PType string `json:"ptype" validate:"required,oneof=sid ip name city loc"`
+	Sid   string `json:"sid" validate:"required_if=PType sid,omitempty,number"`
+	Name  string `json:"name" validate:"required_if=PType name,omitempty,excludesall=@?*%"`
+	City  string `json:"city" validate:"required_if=PType city,omitempty,excludesall=@?*%"`
+	Ip    string `json:"ip" validate:"required_if=PType ip,omitempty,ip4_addr"`
+	Lng   string `json:"lng" validate:"required_if=PType loc,omitempty,longitude"`
+	Lat   string `json:"lat" validate:"required_if=PType loc,omitempty,latitude"`
+}
+
+type StationSearchRequest struct {
+	QType string `json:"qtype" validate:"required,oneof=_search"`
+	PType string `json:"ptype" validate:"required,oneof=name city area"`
+	Size  int    `json:"size" validate:"required,number,min=1,max=10000"`
+	Name  string `json:"name" validate:"required_if=PType name,omitempty,excludesall=@?*%"`
+	City  string `json:"city" validate:"required_if=PType city,omitempty,excludesall=@?*%"`
+}
+
+func (app *AQIServer) StationGet(ctx *fiber.Ctx) error {
+	var query StationGetRequest
+	err := ctx.QueryParser(&query)
+	if err != nil {
+		return FailWithMessage(http.StatusBadRequest, "can't parser params", ctx)
 	}
+	errResp := ValidateStruct(query)
+	if errResp != nil {
+		return FailWithDetailed(http.StatusBadRequest, errResp, "", ctx)
+	}
+	if query.PType == "sid" {
+		return app.GetStationById(query.Sid, ctx)
+	} else if query.PType == "name" {
+		return app.GetStationByName(query.Name, ctx)
+	} else if query.PType == "city" {
+		return app.GetStationByCity(query.City, ctx)
+	} else if query.PType == "loc" {
+		return app.GetStationByCity(query.City, ctx)
+	} else {
+		return app.GetStationById(query.Ip, ctx)
+	}
+}
+
+func (app *AQIServer) StationSearch(ctx *fiber.Ctx) error {
+	var query StationSearchRequest
+	err := ctx.QueryParser(&query)
+	if err != nil {
+		return FailWithMessage(http.StatusBadRequest, "can't parser params", ctx)
+	}
+	errResp := ValidateStruct(query)
+	if errResp != nil {
+		return FailWithDetailed(http.StatusBadRequest, errResp, "", ctx)
+	}
+	if query.PType == "name" {
+		return app.SearchStationByName(query.Name, query.Size, ctx)
+	} else if query.PType == "city" {
+		return app.SearchStationByName(query.City, query.Size, ctx)
+	} else {
+		return nil
+	}
+}
+
+func (app *AQIServer) GetStationById(sid string, ctx *fiber.Ctx) error {
 	st, err := app.DB.GetStationById(sid)
 	if err != nil {
-		return FailWithMessage(500, err.Error(), ctx)
+		return FailWithMessage(http.StatusInternalServerError, err.Error(), ctx)
+	}
+	if st == nil {
+		return OkWithNotFound(fiber.MIMEApplicationJSON, ctx)
 	}
 	return OkWithData(st, ctx)
 }
 
-func (app *AQIServer) GetStationByName(ctx *fiber.Ctx) error {
-	name := ctx.Query("name")
-	valid, err := paramsCheck(name, ctx, reflect.String)
-	if !valid {
-		return err
-	}
+func (app *AQIServer) GetStationByName(name string, ctx *fiber.Ctx) error {
 	st, err := app.DB.GetStationByName(name)
 	if err != nil {
-		return FailWithMessage(500, err.Error(), ctx)
+		return FailWithMessage(http.StatusInternalServerError, err.Error(), ctx)
 	}
 	if st == nil {
-		return OkWithNotFound("application/json", ctx)
+		return OkWithNotFound(fiber.MIMEApplicationJSON, ctx)
 	}
 	return OkWithData(st, ctx)
 }
 
-func (app *AQIServer) GetStationByCityName(ctx *fiber.Ctx) error {
-	city := ctx.Query("city")
-	valid, err := paramsCheck(city, ctx, reflect.String)
-	if !valid {
-		return err
-	}
+func (app *AQIServer) GetStationByCity(city string, ctx *fiber.Ctx) error {
 	st, err := app.DB.GetStationByCityName(city)
 	if err != nil {
-		return FailWithMessage(500, err.Error(), ctx)
+		return FailWithMessage(http.StatusInternalServerError, err.Error(), ctx)
 	}
 	if st == nil {
-		return OkWithNotFound("application/json", ctx)
+		return OkWithNotFound(fiber.MIMEApplicationJSON, ctx)
 	}
 	return OkWithData(st, ctx)
 }
 
-func (app *AQIServer) SearchStationByName(ctx *fiber.Ctx) error {
-	name := ctx.Query("name")
-	size := ctx.Query("size")
-	valid, err := paramsCheck(name, ctx, reflect.String)
-	if !valid {
-		return err
-	}
-	validSize, err := paramsCheck(size, ctx, reflect.Int)
-	if !validSize {
-		return err
-	}
-	sizeInt, _ := strconv.ParseInt(size, 10, 64)
-	st, err := app.DB.SearchStationByName(name, int(sizeInt))
+func (app *AQIServer) SearchStationByName(name string, size int, ctx *fiber.Ctx) error {
+	st, err := app.DB.SearchStationByName(name, size)
 	if err != nil {
-		return FailWithMessage(500, err.Error(), ctx)
-	}
-	if st == nil {
-		return OkWithNotFound("application/json", ctx)
+		return FailWithMessage(http.StatusInternalServerError, err.Error(), ctx)
 	}
 	return OkWithData(st, ctx)
 }
 
-func (app *AQIServer) SearchStationByCityName(ctx *fiber.Ctx) error {
-	city := ctx.Query("city")
-	size := ctx.Query("size")
-	valid, err := paramsCheck(city, ctx, reflect.String)
-	if !valid {
-		return err
-	}
-	validSize, err := paramsCheck(size, ctx, reflect.Int)
-	if !validSize {
-		return err
-	}
-	sizeInt, _ := strconv.ParseInt(size, 10, 64)
-	st, err := app.DB.SearchStationByCityName(city, int(sizeInt))
+func (app *AQIServer) SearchStationByCityName(city string, size int, ctx *fiber.Ctx) error {
+	st, err := app.DB.SearchStationByCityName(city, size)
 	if err != nil {
-		return FailWithMessage(500, err.Error(), ctx)
-	}
-	if st == nil {
-		return OkWithNotFound("application/json", ctx)
+		return FailWithMessage(http.StatusInternalServerError, err.Error(), ctx)
 	}
 	return OkWithData(st, ctx)
 }
