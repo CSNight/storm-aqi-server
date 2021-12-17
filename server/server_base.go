1b46c1c084011e35994871e5da76a0b63ae6dfe4
chensi1 <chensi1@sensetime.com>
2021-12-17 14:37:56 +0800

    add validator

--- server/server_base.go
+++ server/server_base.go
@@ -4,15 +4,12 @@ import (
 	"aqi-server/conf"
 	"aqi-server/db"
 	"aqi-server/middleware"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	jsoniter "github.com/json-iterator/go"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
-	"github.com/pkg/errors"
 	"go.uber.org/zap"
-	"reflect"
-	"strconv"
-	"strings"
 )
 
 type AQIServer struct {
@@ -27,6 +24,8 @@ var json = jsoniter.Config{
 	SortMapKeys:            true,
 	ValidateJsonRawMessage: true,
 }.Froze()
+
+var validate = validator.New()
 
 func New(conf *conf.GConfig) *AQIServer {
 	server := fiber.New(fiber.Config{
@@ -65,18 +64,23 @@ func (app *AQIServer) Close() {
 	app.Log.Info(`elasticsearch api closed`)
 }
 
-func paramsCheck(val string, ctx *fiber.Ctx, t reflect.Kind) (bool, error) {
-	if val == "" {
-		return false, FailWithMessage(400, "empty params", ctx)
-	}
-	if strings.Contains(val, "*") {
-		return false, FailWithMessage(400, "bad params", ctx)
-	}
-	if t == reflect.Int {
-		_, err := strconv.ParseInt(val, 10, 64)
-		if err != nil {
-			return false, FailWithMessage(400, errors.Wrap(err, "bad params").Error(), ctx)
+type ErrorResponse struct {
+	FailedField string
+	Tag         string
+	Value       string
+}
+
+func ValidateStruct(params interface{}) []*ErrorResponse {
+	var errorResponses []*ErrorResponse
+	err := validate.Struct(params)
+	if err != nil {
+		for _, errItem := range err.(validator.ValidationErrors) {
+			var element ErrorResponse
+			element.FailedField = errItem.StructNamespace()
+			element.Tag = errItem.Tag()
+			element.Value = errItem.Param()
+			errorResponses = append(errorResponses, &element)
 		}
 	}
-	return true, nil
+	return errorResponses
 }
