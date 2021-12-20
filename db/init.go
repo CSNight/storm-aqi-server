75c122b72166f0ff77147f0f7b7d5261e903ae46
chensi1 <chensi1@sensetime.com>
2021-12-20 18:22:15 +0800

    add station cache and refresh

--- db/init.go
+++ db/init.go
@@ -4,20 +4,24 @@ import (
 	"aqi-server/conf"
 	"aqi-server/elastic"
 	"context"
+	"github.com/coocood/freecache"
 	pool "github.com/jolestar/go-commons-pool/v2"
 	jsoniter "github.com/json-iterator/go"
 	"github.com/oschwald/geoip2-golang"
 	"go.uber.org/zap"
 	"os"
+	"runtime"
+	"time"
 )
 
 type DB struct {
-	Conf *conf.AQIConfig
-	api  *elastic.EsAPI
-	ipDB *geoip2.Reader
-	pool *pool.ObjectPool
-	log  *zap.SugaredLogger
-	ctx  context.Context
+	Conf  *conf.AQIConfig
+	api   *elastic.EsAPI
+	ipDB  *geoip2.Reader
+	pool  *pool.ObjectPool
+	log   *zap.Logger
+	cache *freecache.Cache
+	ctx   context.Context
 }
 
 var json = jsoniter.Config{
@@ -25,6 +29,8 @@ var json = jsoniter.Config{
 	SortMapKeys:            true,
 	ValidateJsonRawMessage: true,
 }.Froze()
+
+var tick = time.NewTicker(time.Second * 10)
 
 func Init(conf *conf.GConfig, logger *zap.Logger) (*DB, error) {
 	var ctx = context.Background()
@@ -44,17 +50,54 @@ func Init(conf *conf.GConfig, logger *zap.Logger) (*DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	cache := freecache.NewCache(100 * 1024 * 1024)
+
 	return &DB{
-		Conf: conf.AQIConf,
-		api:  elasticApi,
-		ipDB: ipDB,
-		pool: poolEs,
-		log:  logger.Sugar().Named("[DB]"),
-		ctx:  ctx,
+		Conf:  conf.AQIConf,
+		api:   elasticApi,
+		ipDB:  ipDB,
+		pool:  poolEs,
+		cache: cache,
+		log:   logger.Named("\u001B[33m[DB]\u001B[0m"),
+		ctx:   ctx,
 	}, nil
 }
 
+func (db *DB) RefreshCache() {
+	db.loadStations()
+	go func() {
+		for {
+			select {
+			case <-tick.C:
+				db.loadStations()
+			}
+		}
+	}()
+}
+
+func (db *DB) loadStations() {
+	stations, err := db.GetAllStations()
+	if err != nil {
+		db.log.Error("refresh stations cache error:", zap.String("err", err.Error()))
+		return
+	}
+	for _, st := range stations {
+		stb, err := json.Marshal(st)
+		if err != nil {
+			continue
+		}
+		_ = db.cache.Set([]byte(st.Sid), stb, 600)
+	}
+	defer func() {
+		stations = nil
+		runtime.GC()
+	}()
+	db.log.Info("refresh stations cache success")
+}
+
 func (db *DB) Close() {
+	tick.Stop()
 	db.api.Close()
 	db.pool.Close(db.ctx)
 }
