c200fae2f6df528a9248cbc172a83f57d0c2765e
chensi1 <chensi1@sensetime.com>
2021-12-22 17:31:38 +0800

    register forecast route

--- server/server_router.go
+++ server/server_router.go
@@ -3,7 +3,8 @@ package server
 import "github.com/gofiber/fiber/v2"
 
 func (app *AQIServer) Register(root fiber.Router) {
-	root.Get("/station", app.StationGet)
-	root.Get("/stations", app.StationSearch)
-	root.Get("/realtime", app.RealtimeGet)
+	root.Get("/aqi/station", app.StationGet)
+	root.Get("/aqi/stations", app.StationSearch)
+	root.Get("/aqi/realtime", app.RealtimeGet)
+	root.Get("/aqi/forecast", app.ForecastGet)
 }
